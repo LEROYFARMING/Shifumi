0b6f72ad14d49d68510824542bfdcaafc5317d33
LEROYFARMING <leroy.farming@gmail.com>
2025-09-17 11:17:32 +0200

    Win Condition button

--- scripts/shifumi.js
+++ scripts/shifumi.js
@@ -1,20 +1,26 @@
 let btnjeu = document.querySelectorAll('.btnj');
 console.log(btnjeu) // vérification de la sélection des boutons
-const choice = ["Pierre", "Papier", "Ciseaux"];
+const choice = ['Pierre', 'Papier', 'Ciseaux'];
+const condition = ['Win', 'Lose', 'Draw'];
+const score = {win:0, lose: 0, draw:0};
 
 
 btnjeu.forEach(bouton => {
     bouton.addEventListener("click", () => {
-        const Player = bouton.textContent;
-        const Robot = choice(Math.floor(Math.random()*3));
+        const player = bouton.textContent;
+        const robot = choice(Math.floor(Math.random()*3));
         let result;
-        if(Player === Robot) {
-            result = "Draw";
-        } else if("Ciseaux" === "Papier"
-            && "Pierre" === "Ciseaux"
-            && "Papier" === "Pierre") {
-
+        if(player === robot) {
+            result = 'draw';
+        } else if(player === 'Ciseaux' && robot === 'Papier'
+            || player === 'Pierre' && robot === 'Ciseaux'
+            || player === 'Papier' && robot === 'Pierre') {
+                result = 'win';
         }
+        else {
+            result = 'lose';
+        }
+        score[result]++;
     })
 })
 
